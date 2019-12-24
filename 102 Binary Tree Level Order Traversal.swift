f81f4fa14e1b566e7d0ea8d3ab85e8799c27ec94
kirinzer <z841354@gmail.com>
2019-12-24 12:55:47 +0800

    add solution for Binary Tree Level Order Traversal

---
+++ 102 Binary Tree Level Order Traversal.swift
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+class Solution {
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        var stack = Array<TreeNode>()
+        var depthStack = Array<Int>() // save node and depth relation
+        var levelArray = Array<Array<Int>>()
+        guard let root = root else {
+            return levelArray
+        }
+        stack.append(root)
+        depthStack.append(0)
+        while stack.count > 0 {
+            if let n = stack.last {
+                let currentDepth = depthStack.last!
+                print(currentDepth)
+                if (levelArray.count <= currentDepth) {
+                    let level = Array<Int>()
+                    levelArray.append(level)
+                }
+                levelArray[currentDepth].append(n.val)
+                stack.removeLast()
+                depthStack.removeLast()
+                if let right = n.right {
+                    stack.append(right)
+                    depthStack.append(currentDepth+1)
+                }
+                if let left = n.left {
+                    stack.append(left)
+                    depthStack.append(currentDepth+1)
+                }
+            }
+        }
+        return levelArray
+    }
+}
